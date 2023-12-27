1a729d622498e579dc8bee7aa2103813c534d1ba
EvgeniiMokeichev <117139151+DerMakar@users.noreply.github.com>
2023-12-27 04:16:10 +0300

    Create tape.h

---
+++ src/tape.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "tapeinter.h"
+#include "exceptions.h"
+#include "fstream"
+#include <iostream>
+#include <filesystem>
+
+namespace yadro {
+	class DataTape : public TapeInter {
+	public:
+		DataTape(const std::string& file_name);
+
+		DataTape(std::string&& file_name);
+
+		std::vector<int> ReadFile() const override;
+
+		std::vector<int> ReadData(size_t data_length) const override;
+
+		int ReadFirstElement() const override;
+
+		void EraseFirstElement() override;
+
+		void WriteData(const std::vector<int>& data) override;
+
+	};
+} // namespace yadro
